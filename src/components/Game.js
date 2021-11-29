8f2774078e0adfc786ce9c5c1457886cef351101
Ryan Maloney <rypmaloney@gmail.com>
2021-11-29 18:19:11 -0500

    Restructured files. Created Card component.

---
+++ src/components/Game.js
@@ -0,0 +1,15 @@
+
+import '../Game.css';
+import Card from './Card';
+
+
+function Game() {
+  return (
+    <div>
+      <p>something</p>
+      <Card />
+    </div>
+  );
+}
+
+export default Game;
