9d0a189764e53e6e07bd55b04bec88731ecc31bd
Ryan Maloney <rypmaloney@gmail.com>
2021-11-30 21:10:05 -0500

    finished adding the rest of the plants. there are now 12 house plants in the array with photos.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "../Game.css";
 import Card from "./Card";
-import dracaena from "./images/Plants/monstera.jpg"
+
+
+import monstera from "./images/Plants/monstera.jpg"
+import dracaena from "./images/Plants/dracaena.jpg"
+import pancake from "./images/Plants/pilea_peromiodes.jpg"
+import aloe from "./images/Plants/aloe_vera.jpg"
+import snake from "./images/Plants/snake.jpg"
+import fig from "./images/Plants/fig.jpg"
+import ficus from "./images/Plants/ficus.jpg"
+import money from "./images/Plants/money.jpg"
+import philo from "./images/Plants/philodendron.jpg"
+import bonsai from "./images/Plants/bonsai.jpg"
+import rubber from "./images/Plants/rubber.jpg"
+import zz from "./images/Plants/zz.jpg"
 
 class Game extends React.Component{
   constructor(props) {
@@ -14,20 +27,60 @@ class Game extends React.Component{
           id:1,
         },
         {
-          name:"Monstera",
-          image:"",
+          name:"Monstera Deliciosa",
+          image: monstera,
           id:2,
         },
         {
-          name:"Pancake",
-          image:"",
+          name:"Pilea Peromiodes",
+          image: pancake,
           id:3,
         },
         {
-          name:"Cactus",
-          image:"",
-          id:1,
-        }
+          name:"Aloe Vera",
+          image: aloe,
+          id:4,
+        },
+        {
+          name:"Snake Plant",
+          image: snake,
+          id:5,
+        },
+        {
+          name:"Fiddle Leaf Fig",
+          image: fig,
+          id:6,
+        },
+        {
+          name:"Ficus",
+          image: ficus,
+          id:7,
+        },
+        {
+          name:"Money Tree",
+          image: money,
+          id:8,
+        },
+        {
+          name:"Philodendron",
+          image: philo,
+          id:9,
+        },
+        {
+          name:"Bonsai Tree",
+          image: bonsai,
+          id:10,
+        },
+        {
+          name:"Rubber Tree",
+          image: rubber,
+          id:12,
+        },
+        {
+          name:"ZZ Plant",
+          image: zz,
+          id:12,
+        },
       ]
     };
 }
